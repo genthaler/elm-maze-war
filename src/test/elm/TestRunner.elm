73378e402bc15751cf2f0c713eff0e001352a1df
Günther Enthaler <genthaler@users.noreply.github.com>
2016-06-03 12:45:00 +1000

    WIP - Added possibility of console tests

--- src/test/elm/TestRunner.elm
+++ src/test/elm/TestRunner.elm
@@ -12,3 +12,10 @@ tests =
 main : Program Never
 main =
     runSuiteHtml tests
+
+
+
+-- This version runs at the console and returns a non-zero exit code on test failure
+-- main : Program Never
+-- main =
+--     runSuite tests
