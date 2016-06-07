2c767e59c16984e22d792efc1abaa3238b9f3ce1
Günther Enthaler <genthaler@users.noreply.github.com>
2016-06-07 23:12:06 +1000

    Setting up for gh-pages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
   del = require('del'),
   shell = require('gulp-shell'),
+  ghPages = require('gulp-gh-pages'),
   browserSync = require('browser-sync').create();
 require('gulp-submodule')(gulp);
 var main = gulp.submodule('main', {
@@ -15,11 +16,20 @@ gulp.task('main-clean', shell.task('gulp clean', {
 gulp.task('test-clean', shell.task('gulp clean', {
   cwd: 'src/test'
 }));
+gulp.task('clean', ['main-clean', 'test-clean']);
 gulp.task('main-default', shell.task('gulp', {
   cwd: 'src/main'
 }))
 gulp.task('test-default', shell.task('gulp', {
   cwd: 'src/test'
 }));
-gulp.task('clean', ['main-clean', 'test-clean']);
 gulp.task('default', ['main-default', 'test-default'], function() {});
+gulp.task('build', shell.task('gulp compile', {
+  cwd: 'src/main'
+}));
+gulp.task('test', shell.task('gulp test', {
+  cwd: 'src/test'
+}));
+gulp.task('deploy', ['build', 'test'], function() {
+  return gulp.src('src/main/target/**/*').pipe(ghPages());
+});
