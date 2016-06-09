02ac0904a33e2fb50b9ea6336fa7829faed0df5f
Günther Enthaler <genthaler@users.noreply.github.com>
2016-06-09 21:44:30 +1000

    Publishing to versioned folder #31
    I realised I need to actually store each version as its built in order
    to keep it for posterity. The alternative would be to check out
    gh-pages into another folder, add new files to it, etc. Would make it
    hard to test everything locally.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var pjson = require('./package.json'),
   ejson = require('./elm-package.json'),
   gulp = require('gulp'),
+  gutil = require("gulp-util"),
   shell = require('gulp-shell'),
   del = require('del'),
   elm = require('gulp-elm'),
   ghPages = require('gulp-gh-pages'),
   browserSync = require('browser-sync').create(),
+  resources = ['resources/**/*', '!resources/ScreenShot.png'],
   src = ['src/**/*.elm'],
   test = ['src/**/*.elm', 'test/**/*.elm'],
+  dist = 'dist/' + pjson.version,
   browserSyncInit = function() {
     browserSync.init({
       server: {
-        baseDir: "target",
+        baseDir: dist,
         index: "index.html"
       },
       port: 4000,
@@ -28,15 +31,23 @@ gulp.task('reload', ['package'], browserSyncReload);
 gulp.task('pre-server', browserSyncInit);
 // external tasks
 gulp.task('clean', function() {
-  return del(['elm-stuff', 'target']);
+  return del(['elm-stuff', 'target', dist]);
 });
-gulp.task('process-resources', [], function() {
-  return gulp.src(['./resources/*']).pipe(gulp.dest('target'));
+gulp.task('process-resources', function() {
+  return gulp.src(resources).pipe(gulp.dest(dist));
+});
+gulp.task('validate', function() {
+  if (pjson.version != ejson.version) {
+    throw new gutil.PluginError({
+      plugin: 'validate',
+      message: "package.json and elm-package.json versions don't agree"
+    });
+  }
 });
 gulp.task('compile', ['elm-init'], function() {
   return gulp.src('src/Main.elm').pipe(elm.bundle('elm.js', {
     warn: true
-  })).pipe(gulp.dest('target/'));
+  })).pipe(gulp.dest(dist));
 });
 gulp.task('test-compile', ['elm-init'], function() {
   return gulp.src('test/TestRunner.elm').pipe(elm.bundle('TestRunner.js', {
@@ -44,15 +55,16 @@ gulp.task('test-compile', ['elm-init'], function() {
   })).pipe(gulp.dest('target'));
 });
 gulp.task('test', ['test-compile'], function() {
-  return gulp.src('target/TestRunner.js').pipe(shell(
-    ['node target/TestRunner.js']))
+  return gulp.src('target/TestRunner.js ').pipe(shell([
+    'node target/TestRunner.js'
+  ]))
 });
-gulp.task('package', ['process-resources', 'compile', 'test'], noop);
+gulp.task('package', ['validate', 'process-resources', 'compile', 'test'], noop);
 gulp.task('server', ['pre-server', 'process-resources', 'compile'], function() {
   browserSyncReload();
 });
 gulp.task('deploy', ['package'], function() {
-  return gulp.src('./target/**/*').pipe(ghPages());
+  return gulp.src('./dist/**/*').pipe(ghPages({}));
 });
 gulp.task('watch', ['test', 'server'], function() {
   gulp.watch(['resources/**/*', 'src/**/*', 'test/**/*'], ['reload']);
